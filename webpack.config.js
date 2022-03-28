6bb80073a139eda44f8e1098d57bf22f2ece9288
Rhea <rheam97@gmail.com>
2022-03-28 15:44:48 -0400

    webpack service worker not working

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,26 @@
 const path = require('path')
 const webpack = require('webpack')
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"},
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js',
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
             jquery: 'jquery'
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+        })
     ],
     mode: 'development'
 }
